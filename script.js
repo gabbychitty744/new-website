4aa183d747bb6d51ee0efe08f2400919be912031
gabbychitty744 <gabbychitty@gmail.com>
2025-09-05 00:40:14 -0400

    Add script.js for custom JavaScript

---
+++ script.js
@@ -0,0 +1,7 @@
+// script.js
+// Add your custom JavaScript code here
+
+document.addEventListener('DOMContentLoaded', function() {
+  console.log("Script.js loaded and DOM is ready!");
+  // You can add more interactive JS here
+});
