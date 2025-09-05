8096d6d751fdc365a9378f130881741661a29673
gabbychitty744 <gabbychitty@gmail.com>
2025-09-05 00:36:55 -0400

    Add index.js as entry point

---
+++ index.js
@@ -0,0 +1,17 @@
+// Entry point for your website project
+
+console.log("Welcome to your new website!");
+
+/*
+  Add your server or client-side code here.
+  For example, you might use Express for a backend:
+  
+  const express = require('express');
+  const app = express();
+  
+  app.get('/', (req, res) => {
+    res.send('Hello World!');
+  });
+  
+  app.listen(3000, () => console.log('Server running on port 3000'));
+*/
